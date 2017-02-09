9d6be084f97599b67650e5bcfd2efe7bf39d44d6
Artem Govorov <artem.govorov@gmail.com>
2017-02-09 13:18:49 +1000

    Initial commit

---
+++ src/a.spec.ts
@@ -0,0 +1,5 @@
+import {UiState} from './uiState';
+
+it('should work', () => {
+  expect(UiState.Succeeded).toBe(1);
+});
---
+++ src/uiState.ts
@@ -0,0 +1,5 @@
+export const enum UiState {
+  NotAsked = 0,
+  Succeeded = 1,
+  Failed = 2
+}
